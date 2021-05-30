68689d5da1b2ef5893c8bbed5d4ddd80bf738e5f
EJ Hawkins <apphawks@gmail.com>
2021-05-30 14:22:06 -0400

    Initial Commit

---
+++ 2048_clone/_048_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  _048_cloneApp.swift
+//  2048_clone
+//
+//  Created by Emile Hawkins, Jr on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct _048_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
